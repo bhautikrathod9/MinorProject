f8a2fda0b1764d4c2c94b5d547d3ffd419eeb10e
gogo <hitmangogo7@gmail.com>
2025-02-08 02:28:46 +0530

    make different component of country dialog box

--- frontend/src/Components/Header/CountryDrop.js
+++ frontend/src/Components/Header/CountryDrop.js
@@ -2,10 +2,8 @@ import React, { useState } from 'react'
 import Button from '@mui/material/Button';
 import { FaAngleDown } from "react-icons/fa";
 import './CountryDrop.css'
-import Dialog from '@mui/material/Dialog';
-import { FaSearch } from "react-icons/fa";
-import { IoCloseSharp } from "react-icons/io5";
 import '../dialog/CountryDialog.css'
+import CountryDialog from '../dialog/CountryDialog'
 
 const CountryDrop = () => {
   const[OpenModel, setOpenModel] = useState(false)
@@ -22,40 +20,10 @@ const CountryDrop = () => {
         </span>
       </Button>
 
-      <Dialog open={OpenModel} onClose={() => setOpenModel(false)} className='locationModel' >
-        <h4 className='mb-0'>Choose your delivery location</h4>
-        <p>Choose your location and we will specify offer for your location</p>
-        <Button className='close_' onClick={() => setOpenModel(false)}><IoCloseSharp /></Button>
-        
-        <div className='headerSearch w-100'>
-          <input type='text' placeholder='Search your area....'/> 
-          <Button><FaSearch /></Button>
-        </div>
-
-        <ul className='countryList mt-3'>
-          <li><Button onClick={() => setOpenModel(false)}>Argentina</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Australia</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Brazil</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Canada</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>China</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>France</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Germany</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>India</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Italy</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Japan</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Mexico</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Netherlands</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>New Zealand</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Russia</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>South Africa</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>South Korea</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Spain</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Sweden</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>Switzerland</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>United Kingdom</Button></li>
-          <li><Button onClick={() => setOpenModel(false)}>United States</Button></li>
-        </ul>
-      </Dialog>
+      <CountryDialog 
+        open={OpenModel}
+        onClose={() => setOpenModel(false)}
+      />
     </>
   )
 }
--- frontend/src/Components/dialog/CountryDialog.js
+++ frontend/src/Components/dialog/CountryDialog.js
@@ -5,13 +5,13 @@ import Button from '@mui/material/Button';
 import { FaSearch } from "react-icons/fa";
 import { IoCloseSharp } from "react-icons/io5";
 
-const countryDialog = () => {
+const countryDialog = ({ open, onClose}) => {
   return (
     <>
-      <Dialog open={false} className='locationModel' >
+      <Dialog open={open} onClose={onClose} className='locationModel' >
         <h4 className='mb-0'>Choose your delivery location</h4>
         <p>Choose your location and we will specify offer for your location</p>
-        <Button className='close_'><IoCloseSharp /></Button>
+        <Button className='close_' onClick={onClose}><IoCloseSharp /></Button>
         
         <div className='headerSearch w-100'>
           <input type='text' placeholder='Search your area....'/> 
@@ -19,27 +19,27 @@ const countryDialog = () => {
         </div>
 
         <ul className='countryList mt-3'>
-          <li><Button>Argentina</Button></li>
-          <li><Button>Australia</Button></li>
-          <li><Button>Brazil</Button></li>
-          <li><Button>Canada</Button></li>
-          <li><Button>China</Button></li>
-          <li><Button>France</Button></li>
-          <li><Button>Germany</Button></li>
-          <li><Button>India</Button></li>
-          <li><Button>Italy</Button></li>
-          <li><Button>Japan</Button></li>
-          <li><Button>Mexico</Button></li>
-          <li><Button>Netherlands</Button></li>
-          <li><Button>New Zealand</Button></li>
-          <li><Button>Russia</Button></li>
-          <li><Button>South Africa</Button></li>
-          <li><Button>South Korea</Button></li>
-          <li><Button>Spain</Button></li>
-          <li><Button>Sweden</Button></li>
-          <li><Button>Switzerland</Button></li>
-          <li><Button>United Kingdom</Button></li>
-          <li><Button>United States</Button></li>
+          <li><Button onClick={onClose}>Argentina</Button></li>
+          <li><Button onClick={onClose}>Australia</Button></li>
+          <li><Button onClick={onClose}>Brazil</Button></li>
+          <li><Button onClick={onClose}>Canada</Button></li>
+          <li><Button onClick={onClose}>China</Button></li>
+          <li><Button onClick={onClose}>France</Button></li>
+          <li><Button onClick={onClose}>Germany</Button></li>
+          <li><Button onClick={onClose}>India</Button></li>
+          <li><Button onClick={onClose}>Italy</Button></li>
+          <li><Button onClick={onClose}>Japan</Button></li>
+          <li><Button onClick={onClose}>Mexico</Button></li>
+          <li><Button onClick={onClose}>Netherlands</Button></li>
+          <li><Button onClick={onClose}>New Zealand</Button></li>
+          <li><Button onClick={onClose}>Russia</Button></li>
+          <li><Button onClick={onClose}>South Africa</Button></li>
+          <li><Button onClick={onClose}>South Korea</Button></li>
+          <li><Button onClick={onClose}>Spain</Button></li>
+          <li><Button onClick={onClose}>Sweden</Button></li>
+          <li><Button onClick={onClose}>Switzerland</Button></li>
+          <li><Button onClick={onClose}>United Kingdom</Button></li>
+          <li><Button onClick={onClose}>United States</Button></li>
         </ul>
       </Dialog>
     </>
