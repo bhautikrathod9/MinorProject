6f9a14e7e128b68bb254a5430669b235a3cdd08a
gogo <hitmangogo7@gmail.com>
2025-02-09 21:15:54 +0530

    header section completed

--- frontend/src/Pages/Home/index.js
+++ frontend/src/Pages/Home/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import HomeBanner from '../../Components/HomeBanner/HomeBanner'
 
 const Home = () => {
   return (
-   <></>
+   <>
+    <HomeBanner />
+   </>
   )
 }
 
